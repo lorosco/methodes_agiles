9c743ff88eaac0d08a52b702cf73d59aa0c3ac3e
lorosco <leonard.orosco2.0@gmail.com>
2022-12-15 14:43:12 +0100

    helloWorld

---
+++ test.py
@@ -0,0 +1,10 @@
+from io import StringIO
+import sys
+import unittest
+from app import *
+
+class Tests(unittest.TestCase):
+    def test_helloWorld(self):
+        self.assertEqual(True, True)
+
+unittest.main()
