7aa52fbc185be822f22269a263e26b502d0f768e
Clément GUITTENY <clement.guitteny@gmail.com>
2022-12-15 15:13:31 +0100

    Add function 11

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+reductions = []
+
 def helloWorld():
     print("Hello, World!")
 
 helloWorld()
+
+#Data format [1,2,3,4,5...]
+def setReductions(reducts):
+    reductions = reducts
+
+def getReductions():
+    return reductions
