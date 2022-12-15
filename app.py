29b4883883e9385f39fa2234b9acd537613225d2
lorosco <leonard.orosco2.0@gmail.com>
2022-12-15 16:36:50 +0100

    TVA code from country code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 reductions = []
+countryCodeForTva = {'FR': 'FR-TVA', 'EN': 'EN-TVA', 'BE': 'BE-TVA', 'IT': 'IT-TVA'}
 
 def helloWorld():
     return "Hello, World!"
@@ -10,6 +11,16 @@ def displayTVAcodes(listCodes):
 
 for i in displayTVAcodes(['AAA', 'BBB', 'CCC']):
     print(i)
+
+def TVAcodeFromCountryCode(countryCode = "None"):
+    print(countryCode)
+    if(countryCode == "None"):
+        print("Enter country code (example: FR, EN, BE, IT...)")
+        countryCode = input()
+    result = countryCodeForTva[countryCode]
+    return result
+
+print(TVAcodeFromCountryCode())
 
 #Data format [1,2,3,4,5...]
 def setReductions(reducts):
--- test.py
+++ test.py
@@ -2,18 +2,23 @@ from io import StringIO
 import sys
 import unittest
 import app
-
 class Tests(unittest.TestCase):
-    def test_helloWorld(self):
+    def test_1_helloWorld(self):
         self.assertEqual("Hello, World!", app.helloWorld())
 
-    def test_displayTVAcodes(self):
+    def test_6_displayTVAcodes(self):
         TVAcodes = ['AAA','BBB', 'CCC']
-        self.assertEquals(['AAA', 'BBB', 'CCC'], app.displayTVAcodes(TVAcodes))
+        self.assertEqual(['AAA', 'BBB', 'CCC'], app.displayTVAcodes(TVAcodes))
 
     def test_11_isAllReductionsCorrectlySet(self):
         reductions = [3,5,7,10,15]
         app.setReductions(reductions)
         self.assertEqual(app.getReductions(),reductions)
+    
+    def test_7_countryCodeForTVA(self):
+        self.assertEqual(app.TVAcodeFromCountryCode("BE"),"BE-TVA")
+        self.assertEqual(app.TVAcodeFromCountryCode("EN"),"EN-TVA")
+        self.assertEqual(app.TVAcodeFromCountryCode("FR"),"FR-TVA")
+        self.assertEqual(app.TVAcodeFromCountryCode("IT"),"IT-TVA")
 
 unittest.main()
