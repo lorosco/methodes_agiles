459b99bdcb510c837a543e267bc78aaf0ef8e694
Paul B <paulbrunet@live.fr>
2022-12-15 15:22:59 +0100

    calculate_subline

--- app.py
+++ app.py
@@ -2,4 +2,7 @@
 def helloWorld():
     print("Hello, World!")
 
+def calculate_sub_total(line):
+    return line['price'] * line ['quantity']
+
 helloWorld()
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from io import StringIO
 import sys
 import unittest
-from app import *
+import app
 
 class Tests(unittest.TestCase):
     def test_helloWorld(self):
         self.assertEqual(True, True)
+    
+    def test_price_and_quantity(self):
+        line = {"price": 10, "quantity": 2}
+        self.assertEqual(app.calculate_sub_total(line), 20)
+
 
 unittest.main()
