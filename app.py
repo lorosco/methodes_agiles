5bd37e6b9c1165efddaf0f6333efee5f117ff321
Paul B <paulbrunet@live.fr>
2022-12-15 15:46:25 +0100

    askUser for price and quantity

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 
+from sympy import true
+
+
 def helloWorld():
     print("Hello, World!")
 
 def calculate_sub_total(line):
     return line['price'] * line ['quantity']
 
+def askUserToAddPriceAndQuantity():
+    price = 0
+    while True:
+        price = input('Enter price : ')
+        if price.isnumeric:
+            break
+    quantity = 0
+    while True:
+        quantity = input('Enter quantity : ')
+        if quantity.isnumeric:
+            break
+    return {'price': float(price), 'quantity': float(quantity)}
+
+
+
 helloWorld()
+print(calculate_sub_total(askUserToAddPriceAndQuantity()))
