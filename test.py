b5606d0ffda5fae718c1abf37282be3cd511492f
Clément GUITTENY <clement.guitteny@gmail.com>
2022-12-15 15:05:37 +0100

    Add test 11

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from io import StringIO
 import sys
 import unittest
-from app import *
+import app
 
 class Tests(unittest.TestCase):
     def test_helloWorld(self):
         self.assertEqual(True, True)
+
+    def test_11_isAllReductionsCorrectlySet(self):
+        reductions = [3,5,7,10,15]
+        app.setReductions(reductions)
+        self.assertEqual(app.getReductions(),reductions)
 
 unittest.main()
