2dad33c74e1d3844c940d19b74503d6225edac25
lorosco <leonard.orosco2.0@gmail.com>
2022-12-16 12:52:33 +0100

    -1

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 reductions = []
-countryCodeForTva = {'FR': 'FR-TVA', 'EN': 'EN-TVA', 'BE': 'BE-TVA', 'IT': 'IT-TVA'}
+articles = []
+countryCodeForTva = {'FR': 20, 'GB': 10, 'BE': 12, 'CA': 11, 'BA': 0.5}
 
 def helloWorld():
     return "Hello, World!"
 
-print(helloWorld())
+def calculate_sub_total(line):
+    return line['price'] * line ['quantity']
+
+def askUserToAddPriceAndQuantity():
+    price = 0
+    while True:
+        price = input('Enter price : ')
+        if price.isnumeric:
+            break
+    quantity = 0
+    while True:
+        quantity = input('Enter quantity : ')
+        if quantity.isnumeric:
+            break
+    return {'price': float(price), 'quantity': float(quantity)}
 
 def displayTVAcodes(listCodes):
     return listCodes
@@ -13,14 +28,26 @@ for i in displayTVAcodes(['AAA', 'BBB', 'CCC']):
     print(i)
 
 def TVAcodeFromCountryCode(countryCode = "None"):
-    print(countryCode)
     if(countryCode == "None"):
-        print("Enter country code (example: FR, EN, BE, IT...)")
-        countryCode = input()
+        countryCode = input("Enter country code (example: FR, EN, BE, IT...)")
     result = countryCodeForTva[countryCode]
     return result
 
-print(TVAcodeFromCountryCode())
+def itemPriceWithTVA(article):
+    TVA = TVAcodeFromCountryCode(article["countryCode"])
+    priceFlat = calculate_sub_total(article)
+    priceWithTaxe = priceFlat + priceFlat*(float(TVA)/100)
+    return priceWithTaxe
+
+def cartPriceWithTVA(articles):
+    total = 0
+    for i in articles:
+        itemPrice = itemPriceWithTVA(i)
+        # itemPrice = i["price"]*i["quantity"]
+        # TVA = TVAcodeFromCountryCode(i["countryCode"])
+        # totalItemPrice = itemPrice + itemPrice*(float(TVA)/100)
+        total += itemPrice
+    return total
 
 #Data format [1,2,3,4,5...]
 def setReductions(reducts):
@@ -28,3 +55,16 @@ def setReductions(reducts):
 
 def getReductions():
     return reductions
+
+def addArticle(price,qty):
+    articles.append([].append(price,qty))
+
+def getArticles():
+    return articles
+
+if __name__ == '__main__':
+    helloWorld()
+    print(calculate_sub_total(askUserToAddPriceAndQuantity()))
+    print(helloWorld())
+    print(TVAcodeFromCountryCode())
+
